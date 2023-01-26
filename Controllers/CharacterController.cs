242d63b5fade5a2a09d2becbd09c8e8ecc903ff7
Jacob Verburg <smucker45@hotmail.com>
2023-01-26 11:24:17 -0800

    Added Authentication and Authorization with jwt

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,10 +1,12 @@
-﻿using dotnet_rpg.Dtos.Character;
+﻿using System.Security.Claims;
+using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace dotnet_rpg.Controllers;
-
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class CharacterController : ControllerBase
@@ -15,11 +17,12 @@ public class CharacterController : ControllerBase
     {
         _characterService = characterService;
     }
-
+    
     [HttpGet("GetAll")]
     public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
     {
-        return Ok(await _characterService.GetAllCharacters());
+        int userId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+        return Ok(await _characterService.GetAllCharacters(userId));
     }
     
     [HttpGet("{id}")]

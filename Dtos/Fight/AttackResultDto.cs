6305b4fa18105eed6e231c78542453e62b027dc2
Jacob Verburg <smucker45@hotmail.com>
2023-02-06 13:00:21 -0800

    Added Fighting and high scores, course complete

---
+++ Dtos/Fight/AttackResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_rpg.Dtos.Fight;
+
+public class AttackResultDto
+{
+    public string Attacker { get; set; } = String.Empty;
+    public string Opponent { get; set; } = String.Empty;
+    public int AttackerHp { get; set; }
+    public int OpponentHp { get; set; }
+    public int Damage { get; set; }
+}
---
+++ Migrations/20230203215850_FightProperties.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetrpg.Migrations
+{
+    public partial class FightProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterSkill_Skills_SkillId",
+                table: "CharacterSkill");
+
+            migrationBuilder.RenameColumn(
+                name: "SkillId",
+                table: "CharacterSkill",
+                newName: "SkillsId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CharacterSkill_SkillId",
+                table: "CharacterSkill",
+                newName: "IX_CharacterSkill_SkillsId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Defeats",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fights",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Victories",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterSkill_Skills_SkillsId",
+                table: "CharacterSkill",
+                column: "SkillsId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CharacterSkill_Skills_SkillsId",
+                table: "CharacterSkill");
+
+            migrationBuilder.DropColumn(
+                name: "Defeats",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Fights",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Victories",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "SkillsId",
+                table: "CharacterSkill",
+                newName: "SkillId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CharacterSkill_SkillsId",
+                table: "CharacterSkill",
+                newName: "IX_CharacterSkill_SkillId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CharacterSkill_Skills_SkillId",
+                table: "CharacterSkill",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
